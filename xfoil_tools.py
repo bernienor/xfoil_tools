d1320db69ddc36686f682feaaefafd5287528280
Bernt Hustad Hembre <bhhe@hvl.no>
2018-06-19 10:51:30 +0200

    Added SolidWorks curve export

--- xfoil_tools.py
+++ xfoil_tools.py
@@ -12,7 +12,14 @@ class xfoiltools:
     def __init__(self, load_dir='.\\', save_dir='.\\'):
         self.load_dir = load_dir
         self.save_dir = save_dir
-        self.xfoil_input = 'xfoil.inp'
+
+    def output(self, data, file=None):
+        ''' Helper function to simplify writing to file or to stdout '''
+        if(file):
+            file.write(data)
+            file.write('\n')
+        else:
+            print(data)
 
     def Repanel_smooth(self, airfoilname, noofpanels, dz=0, dz_pos=0):
         load_name = self.load_dir + airfoilname + '.dat'
@@ -43,3 +50,19 @@ class xfoiltools:
         for command in xfoil_input:
             commands = commands + bytearray(command, 'utf8')
         run('c:\\bernt\\bin\\xfoil\\xfoil.exe', input=commands)
+
+    def swexport(self, airfoilname, filename=None, Z=0.0):
+        ''' Exports the given airfoil file to a SolidWorks curve line in X, Y
+            plane at the given Z height. Writes to file if a filename is given.
+        '''
+        try:
+            outputfile = open(filename, 'w')
+        except(FileNotFoundError, TypeError):
+            print('\n\nException!!!\n\n')
+            outputfile = None
+        with open(airfoilname) as airfoilfile:
+            next(airfoilfile)  # Skip xfoil header
+            for line in airfoilfile:
+                data = line.split()
+                self.output(str(data[0]) + '\t' + str(data[1]) +
+                            '\t' + str(Z), outputfile)
--- xfoil_tools_example.py
+++ xfoil_tools_example.py
@@ -3,7 +3,8 @@ ag455ct02r.dat origins from http://airfoiltools.com/airfoil/details?airfoil=ag45
 
 '''
 
-
 import xfoil_tools as xfoil
-xf = xfoil.xfoiltools()
-xf.Repanel_smooth('ag455ct02r', 400)
+xf = xfoil.xfoiltools(load_dir='.\\', save_dir='.\\')
+xf.Repanel_smooth('ag455ct02r', 800)
+#xf.swexport(airfoilname='ag455ct02r_refined.dat')
+xf.swexport(airfoilname='ag455ct02r_refined.dat', filename='ag455_sw.txt')
